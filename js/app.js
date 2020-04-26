21379e13990e7815c246427b62bd446bdbd5b13c
dustin <dustinusey@me.com>
2020-04-26 13:16:34 -0500

    Initial commit
    lightbox funtionality in place, need to create search functionality.

---
+++ js/app.js
@@ -0,0 +1,85 @@
+const galleryContainer = document.querySelector('.gallery-container');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.fa-times');
+let currentSelection;
+let galleryHTML = '';
+
+for (let i = 0; i < gallery.length; i++) {
+	galleryHTML += `<img src="${gallery[i].thumb}">`
+}
+galleryContainer.innerHTML = galleryHTML;
+
+
+galleryContainer.addEventListener('click', (e) => {
+	let img = galleryContainer.querySelectorAll('img');
+	for (let i = 0; i < img.length; i++) {
+		if (e.target === img[i]) {
+			overlay.classList.add('show-overlay');
+		}
+	}
+});
+overlay.addEventListener('click', function(e) {
+	if (e.target === this) {
+		this.classList.remove('show-overlay');
+	}
+	if (e.target === close) {
+		this.classList.remove('show-overlay');
+	}
+	if (e.target === imgContainer) {
+		this.classList.remove('show-overlay');
+	}
+});
+
+const imgContainer = document.querySelector('.image-container');
+galleryContainer.addEventListener('click', (e) => {
+	let img = galleryContainer.querySelectorAll('img');
+	for (let i = 0; i < img.length; i++) {
+		if (e.target === img[i]) {
+			currentSelection = i;
+			checkIndex();
+			imgContainer.innerHTML = `<img class="full-image" src="${gallery[i].fullsize}">`
+		}
+	}
+	let fullSizeImage = document.querySelector('.full-image');
+	window.setTimeout(function() {
+		fullSizeImage.classList.add('largeSize');
+	}, 200)
+});
+
+
+const leftArrow = document.querySelector('.fa-arrow-left');
+const rightArrow = document.querySelector('.fa-arrow-right');
+
+leftArrow.addEventListener('click', () => {
+	currentSelection -= 1;
+	imgContainer.innerHTML = `<img class="full-image-filter" src="${gallery[currentSelection].fullsize}">`
+	window.setTimeout(function () {
+		let fullSizeImage = document.querySelector('.full-image-filter');
+		fullSizeImage.classList.add('largeSize');
+		fullSizeImage.style.animation = 'fadeInLeft .7s forwards ease-in-out';
+	}, 200)
+	checkIndex();
+});
+rightArrow.addEventListener('click', () => {
+	currentSelection += 1;
+	imgContainer.innerHTML = `<img class="full-image-filter" src="${gallery[currentSelection].fullsize}">`
+	window.setTimeout(function () {
+		let fullSizeImage = document.querySelector('.full-image-filter');
+		fullSizeImage.classList.add('largeSize');
+		fullSizeImage.style.animation = 'fadeInRight .7s forwards ease-in-out'
+	}, 200)
+	checkIndex();
+});
+
+function checkIndex() {
+	if ( currentSelection === gallery.length - 1 ) {
+		rightArrow.classList.add('disabledArrow');
+	} else {
+		rightArrow.classList.remove('disabledArrow');
+	}
+	if ( currentSelection === 0 ) {
+		leftArrow.classList.add('disabledArrow');
+	} else {
+		leftArrow.classList.remove("disabledArrow");
+	}
+}
---
+++ js/gallery.js
@@ -0,0 +1,61 @@
+const gallery = [
+	{
+		thumb: "img/thumbnails/thumb1.jpg",
+		fullsize: "img/fullsize/fullsize1.jpg",
+	},
+
+	{
+		thumb: "img/thumbnails/thumb2.jpg",
+		fullsize: "img/fullsize/fullsize2.jpg",
+	},
+
+	{
+		thumb: "img/thumbnails/thumb3.jpg",
+		fullsize: "img/fullsize/fullsize3.jpg",
+	},
+
+	{
+		thumb: "img/thumbnails/thumb4.jpg",
+		fullsize: "img/fullsize/fullsize4.jpg",
+	},
+
+	{
+		thumb: "img/thumbnails/thumb5.jpg",
+		fullsize: "img/fullsize/fullsize5.jpg",
+	},
+
+	{
+		thumb: "img/thumbnails/thumb6.jpg",
+		fullsize: "img/fullsize/fullsize6.jpg",
+	},
+
+	{
+		thumb: "img/thumbnails/thumb7.jpg",
+		fullsize: "img/fullsize/fullsize7.jpg",
+	},
+
+	{
+		thumb: "img/thumbnails/thumb8.jpg",
+		fullsize: "img/fullsize/fullsize8.jpg",
+	},
+
+	{
+		thumb: "img/thumbnails/thumb9.jpg",
+		fullsize: "img/fullsize/fullsize9.jpg",
+	},
+
+	{
+		thumb: "img/thumbnails/thumb10.jpg",
+		fullsize: "img/fullsize/fullsize10.jpg",
+	},
+
+	{
+		thumb: "img/thumbnails/thumb11.jpg",
+		fullsize: "img/fullsize/fullsize11.jpg",
+	},
+
+	{
+		thumb: "img/thumbnails/thumb12.jpg",
+		fullsize: "img/fullsize/fullsize12.jpg",
+	}
+];
